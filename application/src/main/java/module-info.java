7c801852c3a4a8c356bfe290910fdaf6886450de
Chris de Vreeze <chris.de.vreeze@caiway.net>
2026-02-07 00:58:06 +0100

    Using Java Modules

---
+++ application/src/main/java/module-info.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025-2025 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Application Java Module, using the service layer, and offering web access to the application.
+ *
+ * @author Chris de Vreeze
+ */
+open module eu.cdevreeze.pagilaapp.application {
+    requires com.google.common;
+    requires org.jooq;
+    requires org.jspecify;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.boot.jooq;
+    requires spring.context;
+    requires spring.tx;
+    requires spring.web;
+
+    requires eu.cdevreeze.pagilaapp.service;
+
+    // No need to export any packages, right?
+}
---
+++ service/src/main/java/module-info.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025-2025 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Service layer Java Module, exposing the service layer as Java interfaces.
+ *
+ * @author Chris de Vreeze
+ */
+open module eu.cdevreeze.pagilaapp.service {
+    requires com.google.common;
+    requires jakarta.annotation;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires org.jooq;
+    requires org.jspecify;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.tx;
+
+    requires transitive eu.cdevreeze.pagilaapp.domain;
+
+    // JPA/Hibernate are internals and not exported
+    // Public service API as Java interfaces
+    exports eu.cdevreeze.pagilaapp.service;
+}
